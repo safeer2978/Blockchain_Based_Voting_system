8f99636039ca0587e0b8e3a499a10124dbf8d948
safeer2978 <safeerkhan2978@gmail.com>
2020-04-03 22:29:01 +0530

    organised everything

---
+++ dump/Voting portal/node_modules/web3-eth-ens/src/config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var config = {
+    addresses: {
+        main: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+        ropsten: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+        rinkeby: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+        goerli: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
+    },
+};
+
+module.exports = config;
